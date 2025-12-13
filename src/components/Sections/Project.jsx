ec6c04438b24032d213827125cc75c08e796cfd4
[Kent Cyrem] <kentcyrem.patasin@gmail.com>
2025-12-13 11:42:32 +0800

    update Project component structure and styles; adjust font sizes in hero section

---
+++ src/components/Sections/Project.jsx
@@ -0,0 +1,21 @@
+import './Project.css';
+
+const Project = () => {
+  return (
+    <section className="project-section" id="projects">
+
+      <div className='project-header'>
+
+      </div>
+
+      <div className='project-content'>
+
+      </div>
+
+    </section>
+  )
+
+}
+
+export default Project;
+
