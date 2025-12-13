579e358becb12700181395eea4e8a516d6ee6b0b
[Kent Cyrem] <kentcyrem.patasin@gmail.com>
2025-12-13 12:21:12 +0800

    add Project section with header and styles for improved layout

--- src/components/Sections/Project.jsx
+++ src/components/Sections/Project.jsx
@@ -5,7 +5,15 @@ const Project = () => {
     <section className="project-section" id="projects">
 
       <div className='project-header'>
-
+        <div className='project-header-container'>
+          <div className='header-divider'></div>
+          <div className='header-title-container'>
+            <h2 className='header-title'>I MAKE PROJECTS</h2>
+            <h2 className='header-title'>FEEL <span id='title-alive'>alive</span></h2>
+            <p className='header-subtitle'>Selected projects</p>
+          </div>
+        </div>
+        
       </div>
 
       <div className='project-content'>
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx'
 
 import Navigation from './components/Layout/navigation.jsx'
 import Hero from './components/Sections/Hero.jsx'
+import Project from './components/Sections/Project.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navigation />
     <Hero />
+    <Project />
     <App />
   </StrictMode>,
 )
