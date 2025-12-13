f92a715efc8ab0a219f6ea7e7f9e8440138baaa1
[Kent Cyrem] <kentcyrem.patasin@gmail.com>
2025-12-14 01:26:13 +0800

    Improved marquee design

--- src/components/Sections/Project.jsx
+++ src/components/Sections/Project.jsx
@@ -6,47 +6,56 @@ const Project = () => {
 
       <div className='project-header'>
         <div className='project-header-container'>
+
           <div className='header-divider'></div>
           <div className='header-title-container'>
             <h2 className='header-title'>I MAKE PROJECTS</h2>
             <h2 className='header-title'>FEEL <span id='title-alive'>alive</span></h2>
             <p className='header-subtitle'>Selected projects</p>
           </div>
-        </div>
-
-        <div className='marquee-wrapper'>
-          <div className='marquee-gradient-left'></div>
-          <div className='marquee-gradient-right'></div>
-          <div className='marquee-track'>
-            <ul className='marquee-content'>
-              <li>WEBSITES</li>
-              <li>PROTOTYPES</li>
-              <li>KIOSK</li>
-              <li>ARTIFICIAL INTELLIGENCE</li>
-              <li>GRAPHICS</li>
-              <li>PUBLICATIONS</li>
-              <li>VECTOR</li>
-              <li>USER INTERFACE</li>
-              <li>USER EXPERIENCE</li>
-            </ul>
-            <ul className='marquee-content' aria-hidden='true'>
-              <li>WEBSITES</li>
-              <li>PROTOTYPES</li>
-              <li>KIOSK</li>
-              <li>ARTIFICIAL INTELLIGENCE</li>
-              <li>GRAPHICS</li>
-              <li>PUBLICATIONS</li>
-              <li>VECTOR</li>
-              <li>USER INTERFACE</li>
-              <li>USER EXPERIENCE</li>
-            </ul>
+          
+          <div className='marquee-wrapper'>
+            <div className='marquee-gradient-left'></div>
+            <div className='marquee-gradient-right'></div>
+            <div className='marquee-track'>
+              <ul className='marquee-content'>
+                <li>WEBSITES</li>
+                <li>PROTOTYPES</li>
+                <li>KIOSK</li>
+                <li>ARTIFICIAL INTELLIGENCE</li>
+                <li>GRAPHICS</li>
+                <li>PUBLICATIONS</li>
+                <li>VECTOR</li>
+                <li>USER INTERFACE</li>
+                <li>USER EXPERIENCE</li>
+              </ul>
+              <ul className='marquee-content' aria-hidden='true'>
+                <li>WEBSITES</li>
+                <li>PROTOTYPES</li>
+                <li>KIOSK</li>
+                <li>ARTIFICIAL INTELLIGENCE</li>
+                <li>GRAPHICS</li>
+                <li>PUBLICATIONS</li>
+                <li>VECTOR</li>
+                <li>USER INTERFACE</li>
+                <li>USER EXPERIENCE</li>
+              </ul>
+            </div>
           </div>
+
         </div>
-        
       </div>
 
       <div className='project-content'>
+        <div className='project-content-container'>
 
+          <div className='project-card-container'>
+            {/* Project cards will be added here in the future */}
+            <p>project here</p>
+          </div>
+
+
+        </div>
       </div>
 
     </section>
